e4d24844f0c70bf3ff1a6044d16caf92cf281728
adkiw <DARIUS.AUKSELIS@GMAIL.COM>
2025-05-28 09:06:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import streamlit as st
-from modules import kroviniai, vilkikai, priekabos, grupes, vairuotojai, klientai, darbuotojai, nustatymai
+from modules import dispo, kroviniai, vilkikai, priekabos, grupes, vairuotojai, klientai, darbuotojai, nustatymai
 from db import init_db
 
 st.set_page_config(layout="wide")
 conn, c = init_db()
 
 # Moduliai
-moduliai = ["dispo", "Kroviniai", "Vilkikai", "Priekabos", "Grupės", "Vairuotojai", "Klientai", "Darbuotojai", "Nustatymai"]
+moduliai = ["Dispo", "Kroviniai", "Vilkikai", "Priekabos", "Grupės", "Vairuotojai", "Klientai", "Darbuotojai", "Nustatymai"]
 modulis = st.sidebar.radio("📂 Pasirink modulį", moduliai)
 
 # Kiekvieno modulio kvietimas
-if modulis == "dispo":
-    st.title("dispo – Planavimo lentelė")
-    st.markdown("dispo modulis dar pildomas.")
+if modulis == "Dispo":
+    dispo.show(conn, c)
 elif modulis == "Kroviniai":
     kroviniai.show(conn, c)
 elif modulis == "Vilkikai":
